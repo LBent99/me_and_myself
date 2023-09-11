b9dc691e6551471754b01420bbb9ce4ad643c514
LBent99 <135572555+LBent99@users.noreply.github.com>
2023-09-10 19:43:42 -0700

    Add files via upload

---
+++ lauries-website/javascript/bootstrap.js
@@ -0,0 +1,9 @@
+const box = document.getElementById("card1")
+const output = document.getElementById("output")
+
+const box = document.getElementById("card2")
+const output = document.getElementById("output")
+
+const box = document.getElementById("card3")
+const output = document.getElementById("output")
+
